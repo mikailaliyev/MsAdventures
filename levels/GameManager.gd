b5cf6ffd47e7fbfce553308a3cfbfad42a005270
mikailaliyev <mikail.aliyev@gmail.com>
2024-05-08 21:46:44 +0400

    upgraded: game control tips for counters and intro for coins&enemies

--- levels/GameManager.gd
+++ levels/GameManager.gd
@@ -1,10 +1,18 @@
 extends Node
+var is_paused: bool = false
 var left_control: int = 0
 var right_control: int = 0
 var jump_control: int = 0
-# Called when the node enters the scene tree for the first time.
+var coin_control: int = 0
+var live_control: int = 0
+var coin_intro_control: int = 0
+var enemy_intro_control: int = 0
+
 func _process(_delta):
-		#Teaching moving controls
+	# Pausing the game
+	#pause_game()
+
+	#Teaching moving controls
 	if left_control == 0:
 		if $"../Character".is_on_floor():
 			if !Input.is_action_pressed("right"):
@@ -40,4 +48,61 @@ func _process(_delta):
 				%LabelForUpControl.hide()
 				%ArrowUp.hide()
 		
+	if coin_control == 0:
+		if %Character.position.x > 540 && right_control > 0 && jump_control > 0:
+			%LabelForCoinsCounter.show()
+			%RectangleForCoins.show()			
+			get_tree().paused = true
+			if Input.is_action_just_pressed("pause"): 
+				get_tree().paused = false
+				coin_control += 1
+				%LabelForCoinsCounter.hide()
+				%RectangleForCoins.hide()
+	
+	if live_control == 0:
+		if %Character.position.x > 550 && coin_control > 0:
+			%LabelForLivesCounter.show()
+			%RectangleForLives.show()
+			if !Input.is_action_pressed("pause"):
+				get_tree().paused = true
+			else: 
+				get_tree().paused = false
+				live_control += 1
+				%LabelForLivesCounter.hide()
+				%RectangleForLives.hide()
+				
+	if coin_intro_control == 0:
+		if %Character.position.x > 2100:
+			%LabelForCoinsIntro.show()
+			%RectangleForCoinsIntro.show()
+			if !Input.is_action_pressed("pause"):
+				get_tree().paused = true
+			else: 
+				get_tree().paused = false
+				coin_intro_control += 1
+				%LabelForCoinsIntro.hide()
+				%RectangleForCoinsIntro.hide()
 
+	if enemy_intro_control == 0:
+		if %Character.position.x > 2800:
+			%LabelForEnemyIntro.show()
+			%RectangleForEnemyIntro.show()
+			if !Input.is_action_pressed("pause"):
+				get_tree().paused = true
+			else: 
+				get_tree().paused = false
+				enemy_intro_control += 1
+				%LabelForEnemyIntro.hide()
+				%RectangleForEnemyIntro.hide()
+
+
+func pause_game():
+	if Input.is_action_just_pressed("pause"):
+		if is_paused:
+			get_tree().paused = false
+			is_paused = false
+			return false
+		else:
+			get_tree().paused = true
+			is_paused = true
+			return true
